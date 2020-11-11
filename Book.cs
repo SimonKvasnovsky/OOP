a8ceb9607e858a1f8200bea964a87f3f91ebb920
Simon <simon.kvasnovsky@gmail.com>
2020-11-11 14:31:41 +0100

    Creating a class and an object from class.

---
+++ Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Open_Lab_10._00
+{
+    class Book
+    {
+        private int Pages;
+        public string Title;
+        public void TitleAndPages()
+        {
+            Console.WriteLine("The title of the book is " + Title + " and it has " + Pages + " pages ");
+        }
+      
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,16 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Open_Lab_10._00
 {
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hi OOP!");
+        {         
+            Book LOTR = new Book();
+            LOTR.Title = "šimon";
+            LOTR.Pages = 55;
+            LOTR.TitleAndPages();
         }
     }
 }
